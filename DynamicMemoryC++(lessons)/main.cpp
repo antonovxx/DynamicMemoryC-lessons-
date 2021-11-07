1aa1c469652ae15001acbb995659e24e9333b08b
antonkurin <antonkurin1@gmail.com>
2021-11-07 03:44:51 +0300

    done

--- DynamicMemoryC++(lessons)/main.cpp
+++ DynamicMemoryC++(lessons)/main.cpp
@@ -2,25 +2,14 @@
 //#define ARRAYS
 #define MATRIX
 
-
-void FillRand(int* array, const int size);
-void FillRand(int** matrix, const int rows, const int cols);
-void Print(int* array, const int size);
-void Print(int** matrix, const int rows, const int cols);
 int** allocate(const int rows, const int cols);
 void clear(int** matrix, const int rows);
 
+void FillRand(int* array, const int size);
+void FillRand(int** matrix, const int rows, const int cols);
 
-int** push_row_back(int** matrix, int& rows, int cols);
-int** push_row_front(int** matrix, int& rows, int cols);
-int** insert_rows(int** matrix, int& rows, int cols, int index);
-int** pop_row_back(int** matrix, int& rows, int cols);
-int** pop_row_front(int** matrix, int& rows, int cols);
-int** erase_row(int** matrix, int& rows, int cols, int index);
-void push_col_back(int** matrix, int rows, int& cols);
-void pop_col_back(int** matrix, int rows, int& cols);
-
-
+void Print(int* array, const int size);
+void Print(int** matrix, const int rows, const int cols);
 
 void Push_back(int* array, int& size, int value);
 void Push_front(int*& array, int& size, int value);
@@ -29,6 +18,25 @@ void pop_back(int*& array, int& size);
 void pop_front(int*& array, int& size);
 int* erase(int* array, int& size, int index);
 
+int** push_row_back(int** matrix, int& rows, int cols);
+int** push_row_front(int** matrix, int& rows, int cols);
+int** insert_rows(int** matrix, int& rows, int cols, int index);
+
+int** pop_row_back(int** matrix, int& rows, int cols);
+int** pop_row_front(int** matrix, int& rows, int cols);
+int** erase_row(int** matrix, int& rows, int cols, int index);
+
+void push_col_back(int** matrix, int rows, int& cols);
+void push_col_front(int** matrix, int rows, int& cols);
+void insert_col(int** matrix, int rows, int& cols, int index = 2);
+
+void pop_col_back(int** matrix, int rows, int& cols);
+void pop_col_front(int** matrix, int rows, int& cols);
+void erase_col(int** matrix, int rows, int& cols, int index = 1);
+
+
+
+
 int main()
 {
     using namespace std;
@@ -36,16 +44,17 @@ int main()
 #ifdef ARRAYS
     int size;
     cout << "Enter size: "; cin >> size;
+    
     int *array = new int[size];
 
     FillRand(array, size);
     Print(array, size);
 
-//    int value;
-//    cout << "Enter value: "; cin >> value;
-//
+    int value;
+    cout << "\nEnter value: "; cin >> value;
+
     int index;
-    cout << "Enter index: ";
+    cout << "\nEnter index: ";
     std::cout << std::endl;
     
     cin >> index;
@@ -71,29 +80,30 @@ int main()
     FillRand(matrix, rows, cols);
     Print(matrix, rows, cols);
     
-    cout << endl;
+//    cout << endl;
     
-    int index;
-    cout << "Enter index: "; cin >> index;
-    
-    cout << endl;
-    
+//    int index;
+//    cout << "\nEnter index: "; cin >> index;
+//
+//    cout << endl;
+//
+//    cout << "\nInsert row: " << endl;
 //    matrix = insert_rows(matrix, rows, cols, index);
+//    Print(matrix, rows, cols);
+//
+//    cout << "\nErase row: "<< endl;
+//
+//    matrix = erase_row(matrix, rows, cols, index);
 //    Print(matrix, rows, cols);
     
     cout << endl;
     
-    matrix = erase_row(matrix, rows, cols, index);
-    Print(matrix, rows, cols);
+//    insert_col(matrix, rows, cols);
+//    Print(matrix, rows, cols);
     
     cout << endl;
-    
-    push_col_back(matrix, rows, cols);
-    Print(matrix, rows, cols);
-    
-    cout << endl;
-    
-    pop_col_back(matrix, rows, cols);
+   
+    erase_col(matrix, rows, cols);
     Print(matrix, rows, cols);
 
     clear(matrix, rows);
@@ -105,6 +115,7 @@ int main()
 
     return 0;
 }
+
 int** allocate(const int rows, const int cols)
 {
     int** matrix = new int*[rows]{};
@@ -112,7 +123,7 @@ int** allocate(const int rows, const int cols)
     {
         matrix[i] = new int[cols]{};
     }
-    return matrix;
+    return matrix; // возвращаем адрес массива на место вызова функции
 }
 
 void clear(int** matrix, const int rows)
@@ -235,6 +246,7 @@ int** erase_row(int** matrix, int& rows, int cols, int index)
     
     return matrix;
 }
+
 void push_col_back(int** matrix, int rows, int& cols)
 {
     for (int i = 0; i < rows; ++i)
@@ -243,6 +255,47 @@ void push_col_back(int** matrix, int rows, int& cols)
         for (int j = 0; j < cols; ++j)
         {
             buffer[j] = matrix[i][j];
+        }
+        delete[] matrix[i];
+        matrix[i] = buffer;
+    }
+    cols ++;
+}
+
+void push_col_front(int** matrix, int rows, int& cols) // добавление в начало
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        int* buffer = new int[cols + 1]{};
+        for (int j = 0; j < cols; ++j)
+        {
+            if(j == 0)
+            {
+                buffer[0] = 0;
+            }
+            buffer[j + 1] = matrix[i][j];
+        }
+        delete[] matrix[i];
+        matrix[i] = buffer;
+    }
+    cols ++;
+}
+
+void insert_col(int** matrix, int rows, int& cols, int index)
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        int* buffer = new int[cols + 1]{};
+        int currentCol = 0;
+        for (int j = 0; j < cols; ++j)
+        {
+            if(j == index)
+            {
+                buffer[index] = 0;
+                currentCol ++;
+            }
+            buffer[currentCol] = matrix[i][j];
+            currentCol ++;
         }
         delete[] matrix[i];
         matrix[i] = buffer;
@@ -263,9 +316,47 @@ void pop_col_back(int** matrix, int rows, int& cols)
         delete [] matrix[i];
         
         matrix[i] = buffer;
-    } 
+    }
     cols --;
 }
+
+void pop_col_front(int** matrix, int rows, int& cols)
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        int* buffer = new int[cols - 1]{};
+        for (int j = 0; j < cols; ++j)
+        {
+            buffer[j] = matrix[i][j + 1];
+        }
+        
+        delete [] matrix [i];
+        
+        matrix [i] = buffer;
+    }
+    cols --;
+}
+
+void erase_col(int** matrix, int rows, int& cols, int index)
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        int* buffer = new int[cols - 1]{};
+        int currentCol = 0;
+        for (int j = 0; j < cols; ++j)
+        {
+            if(j != index)
+            {
+                buffer[currentCol] = matrix[i][j];
+                currentCol ++;
+            }
+        }
+        delete[] matrix[i];
+        matrix[i] = buffer;
+    }
+    cols--;
+}
+
 void FillRand(int* array, const int size)
 {
     for(int i = 0; i < size; ++i)
